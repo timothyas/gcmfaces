a890a0b7d758cbdd8007d692ac8417e46985003c
Timothy Smith <timsmith204@utexas.edu>
2018-09-03 12:22:00 +0200

    Fixes uv fields separately for Pac and Atl views. Needs to be generalized.

--- gcmfaces_maps/m_map_gcmfaces_uv.m
+++ gcmfaces_maps/m_map_gcmfaces_uv.m
@@ -5,6 +5,7 @@ global mygrid;
 if nargin>2; choicePlot=varargin{1}; else; choicePlot=0; end;
 if nargin>3; scaleFac=varargin{2}; else; scaleFac=[]; end;
 if nargin>4; subFac=varargin{3}; else; subFac=1;; end;
+if nargin>5; fontSize=varargin{4}; else; fontSize=14; end;
 
 [fldUe,fldVn]=calc_UEVNfromUXVY(fldU,fldV);
 
@@ -117,5 +118,44 @@ m_coast('patch',[1 1 1]*.7,'edgecolor','none'); m_grid;
 hold on; if isempty(scaleFac); quiver(x,y,u,v); else; quiver(x,y,u,v,scaleFac,'k'); end;
 end;%if choicePlot==0|choicePlot==1; 
 
+% Equatorial Pacific
+if choicePlot==4.5
+
+    % --- mirroring m_map_atl
+    m_proj('mollweide','lat',[-30 30],'lon',[-240 -70]);
+    xtic=[-240:20:-70]; ytic=[-30:10:30];
+
+    [uu,vv]=m_map_gcmfaces_uvrotate(fldUe,fldVn);
+    [xx,yy,u]=convert2pcol(mygrid.XC,mygrid.YC,uu);
+    [xx,yy,v]=convert2pcol(mygrid.XC,mygrid.YC,vv);
+    [x,y]=m_ll2xy(xx,yy);
+
+    %ger rid of nans and subsample:
+    ii=find(~isnan(u.*v)); ii=ii(1:subFac:end);
+    x=x(ii); y=y(ii); u=u(ii); v=v(ii);
+    hold on; if isempty(scaleFac); quiver(x,y,u,v,'k'); else; quiver(x,y,u,v,scaleFac,'k'); end; 
+    m_coast('patch',[1 1 1]*.7,'edgecolor','none'); 
+    m_grid_opt=['''xtick'',xtic,''ytick'',ytic'];
+    eval(['m_grid(' m_grid_opt ');']);
+
+elseif choicePlot==5.2
+    m_proj('mollweide','lat',[-50 -20],'lon',[-60 30]);
+    myConv='pcol';
+    xtic=[-40:20:30]; ytic=[[-50:5:-20]]; xloc='bottom';
 
 
+    [uu,vv]=m_map_gcmfaces_uvrotate(fldUe,fldVn);
+    [xx,yy,u]=convert2pcol(mygrid.XC,mygrid.YC,uu);
+    [xx,yy,v]=convert2pcol(mygrid.XC,mygrid.YC,vv);
+    [x,y]=m_ll2xy(xx,yy);
+    set(gca,'FontSize',fontSize);
+
+    %ger rid of nans and subsample:
+    ii=find(~isnan(u.*v)); ii=ii(1:subFac:end);
+    x=x(ii); y=y(ii); u=u(ii); v=v(ii);
+    hold on; if isempty(scaleFac); quiver(x,y,u,v,'k'); else; quiver(x,y,u,v,scaleFac,'k'); end; 
+    m_coast('patch',[1 1 1]*.7,'edgecolor','none'); 
+    m_grid_opt=['''xtick'',xtic,''ytick'',ytic,''fonts'',fontSize'];
+    eval(['m_grid(' m_grid_opt ');']);
+    set(gca,'FontSize',fontSize);
+end; %if choicePlot==4.5
